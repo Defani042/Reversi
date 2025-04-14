c6d3cfc13904368c7a0fa54ae023c66b3b619e02
Gault115 <gaultierpeyrard2003@gmail.com>
2025-04-14 11:54:59 +0200

    correction de l'etape (j'avais pas compris)

--- src/main.c
+++ src/main.c
@@ -18,7 +18,7 @@ void regles(){
     printf("Règles du reversi : \n");
     printf("\tLe jeu se joue en solo contre l'ordinateur;\n");
     printf("\tAu debut de la partie, le joueur choisit sa couleur ;\n");
-    printf("\tLe joueur jouera toujours en premier peu importe sa couleur ;\n");
+    printf("\tLe joueur choisit s'il veut commencer ou non ;\n");
     printf("\tLes pions de depart sont posés au milieu du plateau ;\n");
     printf("\tLe but du jeu est d'avoir le plus de pions de la couleur du joueur ;\n");
     printf("\tPour se faire, le joueur doit poser un pion de telle sorte qu'il encarde un pion adverse ;\n");
@@ -33,13 +33,13 @@ void aide(char *s){
     printf("%s : Permet de jouer sur une fenêtre.\n\n",s);
     printf("%s --terminal : Permet de jouer sur le terminal.\n\n",s);
     printf("%s --regles : Permet d'afficher les règles du jeu.\n\n",s);
-    printf("%s --niveau val(de 0 à 4) : Choisit la difficulté, par default 2.\n\n",s);
+    printf("%s --niveau val(de 0 à 4) : Choisit l'étape, par default 4.\n\n",s);
     printf("%s --prof val(<0) : Choisit la profondeur de l'arbre pour l'ordinateur pour un difficulté personalisée.\n\n",s);
 }
 
 int main(int argc,char*argv[]){
     int val,index=-1;/*entiers relatifs aux fonctions de getopt.h*/
-    int terminal=0,h=1,r=1,niveau=2,profondeur=0;/*entiers relatifs aux options*/
+    int terminal=0,h=1,r=1,niveau=4,profondeur=0;/*entiers relatifs aux options*/
     
     const char* optstring=":htrn:";
     const struct option lopts[] = {
@@ -60,7 +60,7 @@ int main(int argc,char*argv[]){
         case 'n' : {niveau = atoi(optarg);
                 if (niveau < 0) niveau *= -1;
                 niveau = niveau%5;
-                printf("Le niveau de difficulté a été réglé à %d.\n",niveau);
+                printf("Le niveau de létape est %d.\n",niveau);
                 break;
         }
         case 'p' : {profondeur = atoi(optarg);
